8d2f8c5de9a7d916ce220c682f976ef16041b9c0
haniokasai <htek@haniokasai.com>
2016-12-18 20:12:21 +0900

    done

---
+++ RemoveInventory/RemoveInventory/Class1.cs
@@ -0,0 +1,26 @@
+﻿using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET.Plugins.Commands;
+using MiNET;
+using log4net;
+
+namespace RemoveInventory
+{
+    [Plugin(PluginName = "RemoveInventory", Description = "Remove Inventory for MiNET", PluginVersion = "1.0", Author = "haniokasai")]
+    public class RemoveInventory : Plugin
+    {
+        protected static ILog _log = LogManager.GetLogger("RemoveInventory");
+        protected override void OnEnable()
+        {
+            _log.Warn("Loaded");
+        }
+
+        [Command(Name = "ri", Description = "Remove your Inventory", Permission = "com.haniokasai.RemoveInventory")]
+        public void ri(Player player)
+        {
+            player.Inventory.Clear();
+            player.SendMessage("Removed items from your Inventory!");
+        }
+
+    }
+}
